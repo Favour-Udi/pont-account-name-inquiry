91b42551a739b4dc7d1ce1e56694f0cccc08868d
oghenetega.udi <oghenetega.udi@interswitchng.com>
2025-05-21 17:17:15 +0100

    Adding required folders

--- src/main/java/com/pontai/accountname/app/AppApplication.java
+++ src/main/java/com/pontai/accountname/app/AppApplication.java
@@ -1,4 +1,4 @@
-package com.pontai.mealchoice.app;
+package com.pontai.accountname.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/pontai/accountname/app/AppApplicationTests.java
+++ src/test/java/com/pontai/accountname/app/AppApplicationTests.java
@@ -1,4 +1,4 @@
-package com.pontai.mealchoice.app;
+package com.pontai.accountname.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
